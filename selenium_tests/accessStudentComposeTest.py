8f4d801c6f50c0529fb4557722d9fc8a017cfc48
IsaacPlatypus <Isaac_Ed_C@hotmail.com>
2020-12-13 17:43:50 -0400

    Update accessStudentComposeTest.py

--- selenium_tests/accessStudentComposeTest.py
+++ selenium_tests/accessStudentComposeTest.py
@@ -1,21 +1,23 @@
-# check that in all 3 systems => clicking compose sends you to the studentComposeMain.html
+# Isaac Cain A00391748
+# Check that in all 3 systems => clicking compose sends you to the studentComposeMain.html
 # Need to do so from ALL pages:
-  # inbox, sent items, viewInbox and viewSent for both basic and intermediate BUT viewSent doesn't have a compose link.
-  # mainPage for student advanced
+  # inbox, sent items, and viewInbox for both basic and intermediate.
+  # mainPage for student advanced.
+
 from selenium.webdriver import Chrome
 import time
-import re
 
 index_url = "http://ugdev.cs.smu.ca/~group7/index.html"
 student_email = "StudentTest"
 student_password = "testing"
 compose_system_title = "Your Email System"
-formal_system_url = "http://ugdev.cs.smu.ca/~group7/ComposeScreens/studentComposeFormal.html"
-informal_system_url = "http://ugdev.cs.smu.ca/~group7/ComposeScreens/studentComposeInformal.html"
-basic_viewInbox_url = "http://ugdev.cs.smu.ca/~group7/Student_Basic/viewInbox.html"
-intermediate_viewInbox_url = "http://ugdev.cs.smu.ca/~group7/Student_Intermediate/viewInbox.html"
+formal_system_url = "http://ugdev.cs.smu.ca/~group7/ComposeScreens/studentComposeFormal.html"     # URL for formal compose
+informal_system_url = "http://ugdev.cs.smu.ca/~group7/ComposeScreens/studentComposeInformal.html" # URL for informal compose
+basic_viewInbox_url = "http://ugdev.cs.smu.ca/~group7/Student_Basic/viewInbox.html"               # URL for basic view inbox
+intermediate_viewInbox_url = "http://ugdev.cs.smu.ca/~group7/Student_Intermediate/viewInbox.html" # URL for intermediate view inbox
+intermediateSentItems = "http://ugdev.cs.smu.ca/~group7/Student_Intermediate/sentItems.html"      # URL for intermediate sent items
 
-
+# This function tests the access to compose from the Basic system.
 def testAccessBasic(system, viewInboxURL):
   driver = Chrome()
   driver.get(index_url)
@@ -34,7 +36,7 @@ def testAccessBasic(system, viewInboxURL):
   alert.accept()
   time.sleep(3)
   driver.find_element_by_id("composeButton").click()
-  #formal
+  #formal compose
   driver.find_element_by_id("formalButton").click()
   time.sleep(4)
   assert driver.title == compose_system_title, "wrong compose system title"
@@ -43,7 +45,7 @@ def testAccessBasic(system, viewInboxURL):
   alert = driver.switch_to.alert
   alert.accept()
   time.sleep(3)
-  #informal
+  #informal compose
   driver.find_element_by_id("informalButton").click()
   time.sleep(4)
   assert driver.title == compose_system_title, "wrong compose system title"
@@ -52,7 +54,7 @@ def testAccessBasic(system, viewInboxURL):
   alert = driver.switch_to.alert
   alert.accept()
   time.sleep(3)
-  #inbox
+  #inbox again
   driver.execute_script("javascript:back()")
   time.sleep(4)
   alert = driver.switch_to.alert
@@ -62,7 +64,7 @@ def testAccessBasic(system, viewInboxURL):
   time.sleep(4)
   #sent items
   driver.find_element_by_id("composeButton").click()
-  #formal
+  #formal compose
   driver.find_element_by_id("formalButton").click()
   time.sleep(4)
   assert driver.title == compose_system_title, "wrong compose system title"
@@ -71,7 +73,7 @@ def testAccessBasic(system, viewInboxURL):
   alert = driver.switch_to.alert
   alert.accept()
   time.sleep(3)
-  #informal
+  #informal compose
   driver.find_element_by_id("informalButton").click()
   time.sleep(4)
   assert driver.title == compose_system_title, "wrong compose system title"
@@ -81,9 +83,10 @@ def testAccessBasic(system, viewInboxURL):
   alert.accept()
   time.sleep(3)
   driver.execute_script("javascript:back()")
-  #sent items
+  #sent items again
   time.sleep(3)
   driver.find_element_by_id("inboxButton").click()
+  #inbox again
   time.sleep(3)
   alert = driver.switch_to.alert
   alert.accept()
@@ -97,7 +100,7 @@ def testAccessBasic(system, viewInboxURL):
   assert driver.current_url == viewInboxURL, "wrong viewInbox url"
   driver.find_element_by_id("replyButton").click()
   time.sleep(3)
-  #formal
+  #formal compose
   driver.find_element_by_id("formalButton").click()
   time.sleep(4)
   assert driver.title == compose_system_title, "wrong compose system title"
@@ -106,7 +109,161 @@ def testAccessBasic(system, viewInboxURL):
   alert = driver.switch_to.alert
   alert.accept()
   time.sleep(3)
-  #informal
+  #informal compose
+  driver.find_element_by_id("informalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == informal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #close driver
+  driver.close()
+
+# This function tests compose access from the Intermediate system.
+def testAccessIntermediate(system, viewInboxURL):
+  driver = Chrome()
+  driver.get(index_url)
+  driver.execute_script("javascript:changeToStudent()")
+  time.sleep(3)
+  driver.find_element_by_id('email').send_keys(student_email)
+  driver.find_element_by_id('password').send_keys(student_password)
+  driver.find_element_by_id(system).click()
+  driver.execute_script("javascript:LoginStudent()")
+  time.sleep(3)
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #inbox
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  driver.find_element_by_id("officeButton").click()
+  time.sleep(3)
+  driver.find_element_by_id("composeButton").click()
+  time.sleep(3)
+  #formal compose
+  driver.find_element_by_id("formalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == formal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #informal compose
+  driver.find_element_by_id("informalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == informal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #inbox again
+  driver.execute_script("javascript:back()")
+  time.sleep(4)
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  driver.find_element_by_id("officeButton").click()
+  time.sleep(3)
+  driver.find_element_by_id("sentItemButton").click()
+  time.sleep(3)
+  #sent items
+  assert driver.current_url == intermediateSentItems, "wrong sent items URL"
+  driver.find_element_by_id("officeButton").click()
+  time.sleep(3)
+  driver.find_element_by_id("composeButton").click()
+  time.sleep(3)
+  #formal compose
+  driver.find_element_by_id("formalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == formal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #informal compose
+  driver.find_element_by_id("informalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == informal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  driver.execute_script("javascript:back()")
+  #sent items again
+  driver.find_element_by_id("officeButton").click()
+  time.sleep(3)
+  driver.find_element_by_id("inboxButton").click()
+  #inbox again
+  time.sleep(3)
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #check if there are emails
+  email = driver.find_element_by_id("email0")
+  assert email != None, "email0 not present"
+  driver.execute_script("javascript:viewEmail(0, true)")
+  time.sleep(3)
+  assert driver.current_url == viewInboxURL, "wrong viewInbox url"
+  driver.find_element_by_id("replyButton").click()
+  time.sleep(3)
+  #formal compose
+  driver.find_element_by_id("formalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == formal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #informal compose
+  driver.find_element_by_id("informalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == informal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #close driver
+  driver.close()
+
+# This function tests compose access from the Advanced system.
+def testAccessAdvanced(system):
+  driver = Chrome()
+  driver.get(index_url)
+  driver.execute_script("javascript:changeToStudent()")
+  time.sleep(3)
+  driver.find_element_by_id('email').send_keys(student_email)
+  driver.find_element_by_id('password').send_keys(student_password)
+  driver.find_element_by_id(system).click()
+  driver.execute_script("javascript:LoginStudent()")
+  time.sleep(3)
+  alert = driver.switch_to.alert
+  alert.accept()
+  #main page
+  time.sleep(3)
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  driver.find_element_by_id("composeButton").click()
+  time.sleep(3)
+  #formal compose
+  driver.find_element_by_id("formalButton").click()
+  time.sleep(4)
+  assert driver.title == compose_system_title, "wrong compose system title"
+  assert driver.current_url == formal_system_url, "wrong formal system url"
+  driver.find_element_by_id("cancelButton").click()
+  alert = driver.switch_to.alert
+  alert.accept()
+  time.sleep(3)
+  #informal compose
   driver.find_element_by_id("informalButton").click()
   time.sleep(4)
   assert driver.title == compose_system_title, "wrong compose system title"
@@ -118,6 +275,11 @@ def testAccessBasic(system, viewInboxURL):
   driver.close()
 
 testAccessBasic("Basic", basic_viewInbox_url)
-# works for Basic
-#testAccessBasicIntermediate("Intermediate", intermediate_viewInbox_url)
+time.sleep(4)
+testAccessIntermediate("Intermediate", intermediate_viewInbox_url)
+time.sleep(4)
+testAccessAdvanced("Advanced")
 
+# works for Basic
+# works for Intermediate
+# works for Advanced
