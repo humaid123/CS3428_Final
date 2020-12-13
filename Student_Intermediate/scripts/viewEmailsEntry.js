74349def0e8b2a57b80c346ea375193e3f2c12e6
Lydia Belachew <lydiabelachew12@gmail.com>
2020-12-13 18:15:14 -0400

    added comments

--- Student_Intermediate/scripts/viewEmailsEntry.js
+++ Student_Intermediate/scripts/viewEmailsEntry.js
@@ -1,3 +1,7 @@
+//Commenting: Lydia Belachew (A00416825)
+
+/*This function is resposnible for allowing the user to view the
+contents of an email. If an error occurs then a message will be displayed*/
 function viewEmail(i, isInbox) {
   try {
     var json = { isInbox, index: i };
@@ -8,6 +12,9 @@ function viewEmail(i, isInbox) {
   }
 }
 
+/*This function is responsible for linking to the correct view page.
+If the email is from the inbox, then it goes to the viewInbox page.
+Otherwise, the page is the viewSent emails page.*/
 function linkToCorrectViewPage(isInbox) {
   if (isInbox) {
     window.location.href = "viewInbox.html";
