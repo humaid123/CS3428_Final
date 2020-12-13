5a646c59b603d53025ce3765569fffa1599edcf3
caitlin_30 <caitlin.maillet@smu.ca>
2020-12-13 14:43:42 -0400

    commenting modules

--- Modules/viewEmailComponent.js
+++ Modules/viewEmailComponent.js
@@ -1,3 +1,7 @@
+
+
+/*This function sends the user back from viewing a sent item to the last page they visited
+Written by Caitlin Maillet*/
 function linkBackViewSent() {
   try {
     window.history.back();
@@ -7,17 +11,21 @@ function linkBackViewSent() {
   }
 }
 
+/*This function sends the user back from viewing an inbox item to the last page they visited
+Written by Caitlin Maillet*/
 function linkBackViewInbox() {
   try {
     window.history.back();
-    localStorage.removeItem("emailToView");
+    localStorage.removeItem("emailToView"); //Removes side effects
   } catch (e) {
     alert(e.name + "\n" + e.message);
   }
 }
-
+/* Constants for each part of the email */
 const VIEW_DIVS = ["Cc", "Subject", "Body", "From", "To"];
 
+/*Enables hidden divs that can be clicked to show hints
+Written by Caitlin Maillet*/
 function turnOnHidden(div) {
   for (const curr_div of VIEW_DIVS) {
     if (curr_div == div) {
@@ -38,6 +46,8 @@ function turnOnHidden(div) {
   }
 }
 
+/*This function checks if an email exists in the database, and then calls a method to fill in the fields
+Written by Caitlin Maillet */
 function fillTextBoxes() {
   var isInbox = JSON.parse(localStorage.getItem("emailToView")).isInbox;
   var index = JSON.parse(localStorage.getItem("emailToView")).index;
@@ -50,6 +60,8 @@ function fillTextBoxes() {
   });
 }
 
+/*This function fills the text boxes with the info from the correct email
+Written by Caitlin Maillet*/
 function useEmailToFillTextBoxes(email, isInbox) {
   if (isInbox) {
     $("#partnerTextBox").val(email.from);
@@ -61,6 +73,8 @@ function useEmailToFillTextBoxes(email, isInbox) {
   $("#BodyTextBox").val(email.body);
 }
 
+/*When the reply button is clicked, send student to the compose screen
+Written by Caitlin Maillet*/
 function reply() {
   window.location.href = "../ComposeScreens/studentCompose.html";
 }
