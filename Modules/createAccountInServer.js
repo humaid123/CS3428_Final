019fa21742756ac2fea20e02db939ee8c260c140
IsaacPlatypus <Isaac_Ed_C@hotmail.com>
2020-12-13 13:04:43 -0400

    Update createAccountInServer.js

--- Modules/createAccountInServer.js
+++ Modules/createAccountInServer.js
@@ -1,3 +1,14 @@
+/*
+  Function that creates an account on the server side.
+  Takes as arguments:
+    email: the email address of the account.
+    password: the password for the account.
+    secret: a password that allows the user to create the account.
+    isSpecialist: a boolean to set whether the account is a specialist account.
+    createAccountFunc: the function that creates the account.
+
+  Isaac Cain (A00391748)
+*/
 function createAccountInServer(
   email,
   password,
@@ -5,14 +16,18 @@ function createAccountInServer(
   isSpecialist,
   createAccountFunc
 ) {
+  /* Communicate with the server  using AJAX. */
   $.ajax({
     type: "POST",
     url: SERVER_URL + "/signup",
+    /* Set the data in a JSON */
     data: { email, password, manipulationSecretKey: secret, isSpecialist },
     dataType: "json",
+    /* On success, create the account. */
     success: function (result, status, xhr) {
       createAccountFunc(null, result);
     },
+    /* Else, return an error to the user. */
     error: function (xhrm, status, error) {
       if (xhrm.status != 400) {
         return alert("unrecognised error-status: " + xhrm.status);
